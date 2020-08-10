ca0f1156b329d0e56e71bdad95d8ac7f6aae816b
Gkrumbach07 <gkrumbac@redhat.com>
2020-08-10 14:50:27 -0500

    updated generator func

--- examples/emitter.py
+++ examples/emitter.py
@@ -2,5 +2,8 @@ import requests
 
 def user_defined_function(args):
     response = requests.request("GET", "http://backend:8080/tracked")
-    json.loads(response.text)
+    data = json.loads(response.text)
+    for loc in data["locations"]:
+        yield loc
+
 
