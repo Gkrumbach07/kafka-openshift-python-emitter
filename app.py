e9a29ee8061c4077b557f076762bb42d6f0ac199
Gkrumbach07 <gkrumbac@redhat.com>
2020-08-10 15:26:16 -0500

    bug fix

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def main(args):
     logging.info('beginning producer loop')
     if emitter_function is not None:
         for i in emitter_function(args):
-            producer.send(args.topic, dumps(i).encode('utf-8'))
+            producer.send(args.topic, json.dumps(i).encode('utf-8'))
             time.sleep(1.0 / args.rate)
     logging.info('ending producer loop')
 
