cabff693f32edaa6025bec9c9beed010f5cf41ed
Gage Krumbach <gkrumbach@gmail.com>
2020-08-21 11:27:19 -0500

    Update emitter.py

--- examples/emitter.py
+++ examples/emitter.py
@@ -3,6 +3,7 @@ import json
 
 def user_defined_function(args):
     response = requests.request("GET", "http://backend:8080/tracked")
+    print(response.text)
     data = json.loads(response.text)
     for loc in data["locations"]:
         yield loc
